92284d2d074b79a24d2d737b9835474be26bcf96
Maharana Sarkar <maharanasarkars3@gmail.com>
2023-03-27 00:22:56 +0530

    Added the Password Generation logic in python

---
+++ pass_generator.py
@@ -0,0 +1,21 @@
+import customtkinter
+import random
+
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+sp_char = "!@#$%^&*()<>?:;"
+
+
+def GeneratePassword():
+    string = lower + upper + number + sp_char
+    length = 25
+    password = "".join(random.sample(string, length))
+
+
+    print(password)
+
+
+GeneratePassword()
+
