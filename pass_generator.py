3122dcaa8aecf00a972839b72f2f72d704f632df
Maharana Sarkar <98074344+maharanasarkar@users.noreply.github.com>
2024-01-17 21:34:31 +0530

    Fixed Spacing

--- pass_generator.py
+++ pass_generator.py
@@ -1,20 +1,14 @@
 import random
 
-
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
 sp_char = "!@#$%^&*()<>?:;"
 
-
 def GeneratePassword():
     string = lower + upper + number + sp_char
     length = 25
     password = "".join(random.sample(string, length))
-
-
     print(password)
 
-
 GeneratePassword()
-
